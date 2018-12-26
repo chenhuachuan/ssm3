6c0342271296965dfe4f8787461ba8491a50c5ec
chenhc <2803610695@qq.com>
2018-12-26 20:25:38 +0800

    第二次提交，好嗨喔，好震撼

---
+++ src/test/java/com/baizhi/test/TestGit.java
@@ -0,0 +1,11 @@
+package com.baizhi.test;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("这是第一个测试类，好震撼");
+        /**
+         * 一旦Local Changes里有文件，就需要做提交了，ctrl+k
+         * */
+    }
+}
